8dca9918bc1f5edd29c7bcea17b5b669f417df94
Andrew Caires <andrewcaires@live.com>
2022-04-12 10:42:47 -0300

    remove sqlite

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,100 +1,81 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, UUID, UUIDV4 } from "sequelize";
 
 import { API_DB_DATABASE, API_DB_HOST, API_DB_LOG, API_DB_PASSWORD, API_DB_PORT, API_DB_TYPE, API_DB_USERNAME } from "./config";
 
 const mariadb = (): Sequelize => {
 
-    return new Sequelize(
-        API_DB_DATABASE,
-        API_DB_USERNAME,
-        API_DB_PASSWORD,
-        {
-            host: API_DB_HOST,
-            port: API_DB_PORT,
-            dialect: 'mariadb',
-            logging: API_DB_LOG,
-            define: {
-                timestamps: false
-            }
-        }
-    );
-}
+  return new Sequelize(
+    API_DB_DATABASE,
+    API_DB_USERNAME,
+    API_DB_PASSWORD,
+    {
+      host: API_DB_HOST,
+      port: API_DB_PORT,
+      dialect: "mariadb",
+      logging: API_DB_LOG,
+      define: {
+        timestamps: false,
+      },
+    },
+  );
+};
 
 const mysql = (): Sequelize => {
 
-    return new Sequelize(
-        API_DB_DATABASE,
-        API_DB_USERNAME,
-        API_DB_PASSWORD,
-        {
-            host: API_DB_HOST,
-            port: API_DB_PORT,
-            dialect: 'mysql',
-            logging: API_DB_LOG,
-            define: {
-                timestamps: false
-            }
-        }
-    );
-}
-
-const memory = (): Sequelize => {
-
-    return new Sequelize(
-        'sqlite::memory:',
-        {
-            logging: API_DB_LOG,
-            define: {
-                timestamps: false
-            }
-        }
-    );
-}
+  return new Sequelize(
+    API_DB_DATABASE,
+    API_DB_USERNAME,
+    API_DB_PASSWORD,
+    {
+      host: API_DB_HOST,
+      port: API_DB_PORT,
+      dialect: "mysql",
+      logging: API_DB_LOG,
+      define: {
+        timestamps: false,
+      },
+    },
+  );
+};
 
 const postgres = (): Sequelize => {
 
-    return new Sequelize(
-        API_DB_DATABASE,
-        API_DB_USERNAME,
-        API_DB_PASSWORD,
-        {
-            host: API_DB_HOST,
-            port: API_DB_PORT,
-            dialect: 'postgres',
-            logging: API_DB_LOG,
-            define: {
-                timestamps: false
-            }
-        }
-    );
-}
-
-const sqlite = (): Sequelize => {
-
-    return new Sequelize({
-        storage: API_DB_HOST,
-        dialect: 'sqlite',
-        logging: API_DB_LOG,
-        define: {
-            timestamps: false
-        }
-    });
-}
+  return new Sequelize(
+    API_DB_DATABASE,
+    API_DB_USERNAME,
+    API_DB_PASSWORD,
+    {
+      host: API_DB_HOST,
+      port: API_DB_PORT,
+      dialect: "postgres",
+      logging: API_DB_LOG,
+      define: {
+        timestamps: false,
+      },
+    },
+  );
+};
 
 const create = (): Sequelize => {
 
-    switch (API_DB_TYPE) {
-        case 'mariadb':
-            return mariadb();
-        case 'mysql':
-            return mysql();
-        case 'postgres':
-            return postgres();
-        case 'sqlite':
-            return sqlite();
-    }
+  switch (API_DB_TYPE) {
+  case "mariadb":
+    return mariadb();
+  case "mysql":
+    return mysql();
+  case "postgres":
+    return postgres();
+  }
 
-    return memory();
-}
+  throw new Error("No database selected");
+};
+
+export const uuid = {
+
+  type: UUID,
+  defaultValue: UUIDV4,
+  allowNull: false,
+  primaryKey: true,
+};
 
 export const sequelize = create();
